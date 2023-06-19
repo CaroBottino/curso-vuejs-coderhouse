05a3ad204527a9b5aeb357775ff9bc52241fd094
Caro Bottino <carobottino888@gmail.com>
2023-06-19 19:19:04 -0300

    avances en controles de formulario

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -3,37 +3,107 @@
     Formulario
 
     <form @submit.prevent="submitHandler">
-      <label for="name">Name</label>
-      <input type="text" id="name" v-model="name" />
-      <button>Enviar</button>
-    </form>
-
-    <form>
       <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
+        <label for="name">Name</label>
+        <input
+          type="text"
+          class="form-control"
+          name="name"
+          id="name"
+          v-model="name"
+          placeholder="Enter your name"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <label for="surname">Surname</label>
+        <input
+          type="text"
+          class="form-control"
+          id="surname"
+          name="surname"
+          v-model="surname"
+          placeholder="Enter your surname"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <label for="email">Email address</label>
         <input
           type="email"
           class="form-control"
-          id="exampleInputEmail1"
+          id="email"
+          name="email"
+          v-model="email"
           aria-describedby="emailHelp"
           placeholder="Enter email"
+          required
         />
         <small id="emailHelp" class="form-text text-muted"
           >We'll never share your email with anyone else.</small
         >
       </div>
       <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
+        <label for="password">Password</label>
         <input
           type="password"
           class="form-control"
-          id="exampleInputPassword1"
+          id="password"
+          name="password"
+          v-model="password"
           placeholder="Password"
+          required
         />
       </div>
       <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        <label for="">Document type</label>
+        <div class="row">
+          <div class="col">
+            <input
+              type="radio"
+              name="documentType"
+              id="documentDNI"
+              v-model="document"
+              value="dni"
+            />
+            <label for="documentDNI">DNI</label>
+          </div>
+
+          <div class="col">
+            <input
+              type="radio"
+              name="documentType"
+              id="documentPassport"
+              v-model="document"
+              value="passport"
+            />
+            <label for="documentPassport">Passport</label>
+          </div>
+
+          <div class="col">
+            <input
+              type="radio"
+              name="documentType"
+              id="documentVisa"
+              v-model="document"
+              value="visa"
+            />
+            <label for="documentVisa">VISA</label>
+          </div>
+        </div>
+      </div>
+      <div class="form-check">
+        <input
+          type="checkbox"
+          class="form-check-input"
+          name="legal"
+          id="legal"
+          v-model="legal"
+          required
+        />
+        <label class="form-check-label" for="legal"
+          >I accept the privacy policies</label
+        >
       </div>
       <button type="submit" class="btn btn-primary">Submit</button>
     </form>
@@ -46,15 +116,33 @@ export default {
   data() {
     return {
       name: "",
+      surname: "",
+      email: "",
+      password: "",
+      document: "",
+      legal: false,
     };
   },
   methods: {
     submitHandler() {
-      if (this.name === "") {
-        alert("El nombre es obligatorio");
+      if (this.document === "") {
+        alert("Document type must be selected");
         return;
       }
-      alert("Formulario enviado");
+
+      if (!this.checkPassword(this.password)) {
+        alert("Password must be...");
+        return;
+      }
+
+      alert("Form sent");
+    },
+    checkPassword(pass) {
+      if (pass.length < 8) {
+        return false;
+      }
+
+      return true;
     },
   },
 };
@@ -64,5 +152,9 @@ export default {
 .form-demo {
   margin-left: 20%;
   margin-right: 20%;
+}
+
+label {
+  padding: 5px;
 }
 </style>
