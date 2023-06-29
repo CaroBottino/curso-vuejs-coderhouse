7b534bbe058590033d6b4339cf8b87e15dc547f7
Caro Bottino <carobottino888@gmail.com>
2023-06-28 21:09:39 -0300

    axios incorporado

---
+++ src/controllers/MockapiController.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const BASE_URL = "https://649b334abf7c145d023a2086.mockapi.io/v1";
+
+export default {
+    getItems() {
+        return axios.get(`${BASE_URL}/items`);
+    },
+    getUsers() {
+        return axios.get(`${BASE_URL}/users`);
+    },
+    getUserById(id) {
+        return axios.get(`${BASE_URL}/users/${id}`);
+    },
+    createUser(user) {
+        return axios.post(`${BASE_URL}/users`, user);
+    },
+    updateUser(id, user) {
+        return axios.put(`${BASE_URL}/users/${id}`, user);
+    },
+};
--- src/views/UserPage.vue
+++ src/views/UserPage.vue
@@ -1,24 +1,71 @@
 <template>
   <div class="body">
     <h1>User info page</h1>
-    <h4>Cargar info de usuario id: {{ id }}</h4>
+    <div class="col d-flex justify-content-center">
+      <div class="card">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img :src="user.avatar" class="img-fluid rounded-start" alt="..." />
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">{{ user.fullname }}</h5>
+              <p class="card-text">email: {{ user.email }}</p>
+              <p class="card-text">role: {{ user.role }}</p>
+              <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import MockapiController from "@/controllers/MockapiController";
+
 export default {
   name: "UserPage",
   data() {
     return {
       id: this.$route.params.id,
+      user: {},
     };
+  },
+  created() {
+    this.getUserInfo();
+  },
+  methods: {
+    getUserInfo() {
+      MockapiController.getUserById(this.id).then((res) => {
+        this.user = res.data;
+      });
+    },
   },
 };
 </script>
 
 <style scoped>
 .body {
-  margin-top: 130px;
+  margin-top: 80px;
   margin-bottom: 70px;
+  color: whitesmoke;
+  align-content: center;
+  background-color: #222222;
+}
+
+.card {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  color: black;
+}
+
+img {
+  min-height: 12rem;
+  min-width: 12rem;
+  padding: 1rem;
 }
 </style>
