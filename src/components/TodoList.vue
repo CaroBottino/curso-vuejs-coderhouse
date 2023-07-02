8881c246da3468e9f631c3f658a4719cde25db2a
Caro Bottino <carobottino888@gmail.com>
2023-07-01 21:42:38 -0300

    primer version de todolist

---
+++ src/components/TodoList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>TODO LIST</h1>
+
+    <form @submit.prevent="submitHandler">
+      <input v-model="todo" />
+      <button>Add todo</button>
+    </form>
+
+    <hr />
+
+    <ul>
+      <li v-for="todo in getTodoList" :key="todo.timestamp">{{ todo.text }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "TodoList",
+  data() {
+    return {
+      todo: "",
+    };
+  },
+  computed: {
+    ...mapGetters(["getTodoList"]),
+  },
+  methods: {
+    submitHandler() {
+      this.$store.dispatch("addTodoAction", this.todo);
+      this.todo = "";
+    },
+  },
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,26 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+  state: {
+    todoList: [],
+  },
+  getters: {
+    getTodoList: (state) => {
+      return state.todoList;
+    },
+  },
+  mutations: {
+    addTodo: (state, todo) => {
+      state.todoList.push(todo);
+    },
+  },
+  actions: {
+    addTodoAction: (context, payload) => {
+      context.commit("addTodo", {
+        timestamp: Date.now(),
+        text: payload,
+      });
+    },
+  },
   modules: {},
 });
